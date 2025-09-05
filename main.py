87cf8d3e26a6c3e7d89fb75c80ee4571e2517faa
masoud farahani <masoudfa24121387@gmail.com>
2025-09-05 07:47:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ from os import system
 
 
 def solve_two_drage(a, b, c):
+	try:
 
-	delta = (b**2)-(4*a*c)
+		delta = (b**2)-(4*a*c)
 
-	if delta > 0:
-		x1 = -b+sqrt(delta)/2*a
+		if delta > 0:
+			x1 = -b+sqrt(delta)/2*a
 
-		x2= -b-sqrt(delta)/ 2*a
+			x2= -b-sqrt(delta)/ 2*a
 
-		print(x1,x2)
-
-
-
-
-	elif delta==0:
-		x= -b/2*a
-
-		print(x)
+			print(x1,x2)
 
 
-	else:
 
-		print("delta<0")
+
+		elif delta==0:
+			x= -b/2*a
+
+			print(x)
+
+
+		else:
+
+			print("delta<0")
+
+	except (ValueError, TypeError):
+		print("your values has incorrect")
 
 
 
